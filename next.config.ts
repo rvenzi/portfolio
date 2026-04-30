ac60f73598c55e287c01a76837e6691ec67262a0
RenanVenzi <renanvenzidemoura@gmail.com>
2026-04-30 16:04:10 -0300

    Update next.config.ts to define configuration options for export and image handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
+}; 
